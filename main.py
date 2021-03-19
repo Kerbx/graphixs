3427abeb0ac2db3fe9147e323d143d38bfae5d9e
Kerbx <r3ostat@ya.ru>
2021-03-19 13:46:15 +0300

    Created a main file that contains main code. Created main function, some documentation.

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+This project is a calculator with a simple graphics.
+I want it to be done.
+I hope i will finish it.
+
+
+"""
+import decimal as dcml
+import tkinter as tk
+
+
+def main():
+    """
+    Main function that contains all of the main graphics as
+    window, buttons, out-print window.
+    """
+
+    pass
+
+
+main()
