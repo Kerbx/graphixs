35a0df8dc77861c0c631531bef2bba1de01421c5
Kerbx <r3ostat@ya.ru>
2021-03-19 14:25:06 +0300

    Created list for buttons. Created main window.

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ I hope i will finish it.
 
 
 """
-import decimal as dcml
+import sys
+import math
+import decimal as dcml # Using module for more accuracy that float has.
 import tkinter as tk
+
+
+# List with names for buttons.
+buttons = ['1', '2', '3',
+            '4', '5', '6',
+            '7', '8', '9',
+            '0' '+', '-', '/', '*']
 
 
 def main():
@@ -15,7 +24,13 @@ def main():
     window, buttons, out-print window.
     """
 
-    pass
+    # Creating main window and give a title to it.
+    window = tk.Tk()
+    window.title('Чо это.')
 
+    # This loop automatically creating buttons using "buttons" list.
+    for i in buttons:
+        pass
+        
 
 main()
